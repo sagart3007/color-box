942ba0646f661184590b96ec980bb1ce5578a1f0
sagart3007 <116880569+sagart3007@users.noreply.github.com>
2022-11-23 11:17:06 +0000

    Create Input.js

---
+++ Input.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Input = ({ colorValue, setColorValue }) => {
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
+    >
+      <label>Add color name:</label>
+      <input
+        autoFocus
+        type="text"
+        placeholder="Add color name"
+        required
+        value={colorValue}
+        onChange={(e) => {
+          setColorValue(e.target.value);
+        }}
+      ></input>
+    </form>
+  );
+};
+
+export default Input;
