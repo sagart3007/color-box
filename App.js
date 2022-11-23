ff8c8b5f5fc87906183a143fc62378da6ff4a458
sagart3007 <116880569+sagart3007@users.noreply.github.com>
2022-11-23 11:17:48 +0000

    Create App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+import Square from "./Square";
+import Input from "./Input";
+
+const App = () => {
+  const [colorValue, setColorValue] = useState("");
+  return (
+    <div>
+      <Square colorValue={colorValue} />
+      <Input colorValue={colorValue} setColorValue={setColorValue} />
+    </div>
+  );
+};
+
+export default App;
